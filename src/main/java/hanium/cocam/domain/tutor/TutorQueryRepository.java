1864a7d6fc604ef7e820f7709204eeb0cae75207
jskim <juseog0373@naver.com>
2024-08-11 19:33:41 +0900

    ♻️ refactor: 도메인 패키지 추가 리팩토링

--- src/main/java/hanium/cocam/domain/tutor/TutorQueryRepository.java
+++ src/main/java/hanium/cocam/domain/tutor/TutorQueryRepository.java
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static hanium.cocam.user.entity.QProfile.profile;
-import static hanium.cocam.user.entity.QUser.user;
+import static hanium.cocam.domain.user.entity.QProfile.profile;
+import static hanium.cocam.domain.user.entity.QUser.user;
 
 @Repository
 @Slf4j
